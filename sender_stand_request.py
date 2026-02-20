7573b2fe12576beffe432442dbc5557ae9b573b4
Alexis Coello <co.alexisco@gmail.com>
2026-02-19 19:38:10 -0500

    Mi primer commit

---
+++ sender_stand_request.py
@@ -0,0 +1,127 @@
+import configuration
+import requests
+import data
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,
+        params={"count":20})
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+response = get_users_table()
+print(response.status_code)
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.json())
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,
+                         headers=data.headers)
+
+
+response = post_products_kits(data.product_ids);
+print(response.status_code)
+print(response.json())
+
+# Prueba 2. Creación de un nuevo usuario o usuaria
+# El parámetro "firstName" contiene 15 caracteres
+def test_create_user_15_letter_in_first_name_get_success_response():
+    positive_assert("Aaaaaaaaaaaaaaa")
+
+# Función de prueba negativa
+def negative_assert_symbol(first_name):
+    # El cuerpo de la solicitud actualizada se guarda en la variable user_body
+    user_body = get_user_body(first_name)
+
+    # Comprueba si la variable "response" almacena el resultado de la solicitud.
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Comprueba si la respuesta contiene el código 400.
+    assert response.status_code == 400
+
+    # Comprueba si el atributo "code" en el cuerpo de respuesta es 400.
+    assert response.json()["code"] == 400
+    # Comprueba si el atributo "message" en el cuerpo de respuesta se ve así:
+    assert response.json()["message"] == "Has introducido un nombre de usuario no válido. " \
+                                         "El nombre solo puede contener letras del alfabeto latino, "\
+                                         "la longitud debe ser de 2 a 15 caracteres."
+
+
+def test_create_user_1_letter_in_first_name_get_error_response():
+    negative_assert_symbol("A")
+
+# Prueba 4. Error
+# El parámetro "firstName" contiene 16 caracteres
+def test_create_user_16_letter_in_first_name_get_error_response():
+    negative_assert_symbol("Aaaaaaaaaaaaaaaa")
+
+# Prueba 5. Error
+# El parámetro "firstName" contiene palabras con espacios
+def test_create_user_has_space_in_first_name_get_error_response():
+    negative_assert_symbol("A Aaa")
+
+# Prueba 6. Error
+# El parámetro "firstName" contiene un string de caracteres especiales
+def test_create_user_has_special_symbol_in_first_name_get_error_response():
+    negative_assert_symbol("\"№%@\",")
+
+def test_create_user_has_number_in_first_name_get_error_response():
+    negative_assert_symbol ("123")
+
+# Función de prueba negativa
+# La respuesta contiene el siguiente mensaje de error: "No se han enviado todos los parámetros requeridos"
+def negative_assert_no_firstname(user_body):
+    # Guarda el resultado de llamar a la función a la variable "response"
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Comprueba si la respuesta contiene el código 400
+    assert response.status_code == 400
+
+    # Comprueba si el atributo "code" en el cuerpo de respuesta es 400
+    assert response.json()["code"] == 400
+
+    # Comprueba si el atributo "message" en el cuerpo de respuesta se ve así:
+    assert response.json()["message"] == "No se han aprobado todos los parámetros requeridos"
+
+# Prueba 8. Error
+# La solicitud no contiene el parámetro "firstName"
+def test_create_user_no_first_name_get_error_response():
+    # El diccionario con el cuerpo de la solicitud se copia del archivo "data" a la variable "user_body"
+    # De lo contrario, se podrían perder los datos del diccionario de origen
+    user_body = data.user_body.copy()
+    # El parámetro "firstName" se elimina de la solicitud
+    user_body.pop("firstName")
+    # Comprueba la respuesta
+    negative_assert_no_firstname(user_body)
+
+# Prueba 9. Error
+# El parámetro "firstName" contiene un string vacío
+def test_create_user_empty_first_name_get_error_response():
+    # El cuerpo de la solicitud actualizada se guarda en la variable user_body
+    user_body = get_user_body("")
+    # Comprueba la respuesta
+    negative_assert_no_firstname(user_body)
+
+# Prueba 10. Error
+# El tipo del parámetro "firstName" es un número
+def test_create_user_number_type_first_name_get_error_response():
+    # El cuerpo de la solicitud actualizada se guarda en la variable user_body
+    user_body = get_user_body(12)
+    # El resultado de la solicitud para crear un nuevo usuario o usuaria se guarda en la variable response
+    response = sender_stand_request.post_new_user(user_body)
+
+    # Comprobar el código de estado de la respuesta
+    assert response.status_code == 400
+
+
+
+
+
